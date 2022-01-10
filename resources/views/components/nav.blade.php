5311e9a476f42bd1464a3cf8a662dfe97a767cba
Mikel Martín <mikelmartin43@gmail.com>
2022-01-10 10:06:57 +0100

    auth

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -4,12 +4,32 @@
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
+    <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="/hoteles">Hoteles</a>
         </li>
       </ul>
+      <div class="d-flex gap-3">
+          @auth
+            <div class="d-flex gap-3">
+              {{-- <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a> --}}
+              <form method="POST" action="{{ route('logout') }}">
+                @csrf
+                <x-jet-dropdown-link href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                                this.closest('form').submit();">
+                    {{ __('Log Out') }}
+                </x-jet-dropdown-link>
+              </form>
+            </div>
+          @else
+            <a href="/login" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+            @if (Route::has('register'))
+                <a href="/register" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+            @endif
+          @endauth
+      </div>
     </div>
   </div>
 </nav>
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,12 @@
 
 use App\Http\Controllers\HotelController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 // HOTELES
 Route::get('/', [HotelController::class, 'index']);
 Route::get('/hoteles', [HotelController::class, 'show']);
 
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
