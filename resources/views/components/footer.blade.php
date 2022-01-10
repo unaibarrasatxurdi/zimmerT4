468bfcb56730f952f86ca33866defd178e3cbfdb
unaibarrasatxurdi <unaibarrasatxurdi@gmail.com>
2022-01-10 14:15:24 +0100

    FooterSinTerminar

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,31 @@
+
+<body class="d-flex flex-column">
+  <div id="page-content">
+    <div class="container text-center">
+      <div class="row justify-content-center">
+        <div class="col-md-7">
+          <h1 class="fw-light mt-4 text-white">Sticky Footer using Flexbox</h1>
+          <p class="lead text-white-50">Use just two Bootstrap utility classes and three custom CSS rules and you will have a flexbox enabled sticky footer for your website!</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <footer id="sticky-footer" class="flex-shrink-0 py-4 bg-dark text-white-50">
+  <head>
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" />
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
+</head>
+    <div class="container text-center">
+      <h1>Zimmer</h1>
+      <small>Copyright &copy; Your Website</small>
+      <ul class="social-icons mb-md">
+          <li class="social-icons-fa-github"><a href="https://github.com" target="_blank" title="GitHub"><i class="fa fa-github"></i></a>
+          </li>
+          <li class="social-icons-instagram"><a href="www.linkedin.com" target="_blank" title="Linkedin"><i class="fa fa-instagram"></i></a>
+          </li>
+          <li class="social-icons-stack-overflow"><a href="http://stackoverflow.com" target="_blank" title="Linkedin"><i class="fa fa-stack-overflow"></i></a>
+          </li>
+        </ul>
+    </div>
+  </footer>
+</body>
