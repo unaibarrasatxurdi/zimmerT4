1c5dafaead4ae35d9b79166e564f446a7aec2ed1
Mikel Martín <mikelmartin43@gmail.com>
2022-01-11 08:31:07 +0100

    font awesome

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -5,4 +5,7 @@
 <!-- BOOTSTRAP -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <!-- Styles -->
-<link href="{{ asset('css/main.css') }}" rel="stylesheet">
+<link href="{{ asset('css/main.css') }}" rel="stylesheet">ç
+<!-- Font Awesome -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
