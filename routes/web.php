784e812931381a2f87cdf1c7d0d9f730ec057f38
Mikel Martín <mikelmartin43@gmail.com>
2022-01-11 11:37:45 +0100

    hey

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\HotelController;
-use App\Http\Controllers\CommentController;
 
 /*
 |--------------------------------------------------------------------------
