b8abc776f25c75c0e66df10db9192ec522ca6777
Mikel Martín <mikelmartin43@gmail.com>
2022-01-11 08:09:21 +0100

    base

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,18 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    @include('components.header')
-</head>
-<body>
-    @include('components.nav')
-    <div id="app">
-        @yield('content')
-    </div>
-    <script src="{{ asset('js/app.js') }}"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-</body>
-
-<footer>
-@include('components.footer')
-</footer>
+    <head>
+        @include('components.header')
+    </head>
+    <body>
+        @include('components.nav')
+        <div id="app">
+            @yield('content')
+        </div>
+        <script src="{{ asset('js/app.js') }}"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <footer>
+        @include('components.footer')
+        </footer>
+    </body>
 </html>
