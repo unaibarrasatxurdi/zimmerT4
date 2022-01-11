c6e46add205e6257edd8cfed7dee09f1e0388e53
unaibarrasatxurdi <unaibarrasatxurdi@gmail.com>
2022-01-11 13:37:20 +0100

    MasCambiosFooter

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,11 +3,11 @@
     <h1>Zimmer</h1>
     <small>Copyright &copy; Your Website</small>
     <ul class="social-icons mb-md">
-        <li class="social-icons-fa-github"><a href="https://github.com" target="_blank" title="GitHub"><i class="fa fa-github"></i></a>
+        <li class="social-icons-fa-github"><a href="https://github.com/unaibarrasatxurdi/zimmerT4" target="_blank" title="GitHub"><i class="fa fa-github"></i></a>
         </li>
-        <li class="social-icons-instagram"><a href="www.linkedin.com" target="_blank" title="Linkedin"><i class="fa fa-instagram"></i></a>
+        <li class="social-icons-instagram"><a href="www.instagram.com" target="_blank" title="instagram"><i class="fa fa-instagram"></i></a>
         </li>
-        <li class="social-icons-stack-overflow"><a href="http://stackoverflow.com" target="_blank" title="Linkedin"><i class="fa fa-stack-overflow"></i></a>
+        <li class="social-icons-twitter"><a href="http://twitter.com" target="_blank" title="twitter"><i class="fa fa-twitter"></i></a>
         </li>
     </ul>
   </div>
