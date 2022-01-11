b50fdcb3ca95eb1f8da29569d84af3455b2a915f
Mikel Martín <mikelmartin43@gmail.com>
2022-01-11 08:30:07 +0100

    footer fixed

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,21 +1,14 @@
-
-<body class="d-flex flex-column">
-  <footer id="sticky-footer" class="flex-shrink-0 py-4 bg-dark text-white-50">
-  <head>
-  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" />
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
-</head>
-    <div class="container text-center">
-      <h1>Zimmer</h1>
-      <small>Copyright &copy; Your Website</small>
-      <ul class="social-icons mb-md" id="FooterLine">
-          <li class="social-icons-fa-github"><a href="https://github.com" target="_blank" title="GitHub"><i class="fa fa-github"></i></a>
-          </li>
-          <li class="social-icons-instagram"><a href="https://www.instagram.com/" target="_blank" title="Instagram"><i class="fa fa-instagram"></i></a>
-          </li>
-          <li class="social-icons-twitter"><a href="http://twitter.com" target="_blank" title="twitter"><i class="fa fa-twitter"></i></a>
-          </li>
-        </ul>
-    </div>
-  </footer>
-</body>
+<footer class="text-center bg-dark text-light">
+  <div class="container">
+    <h1>Zimmer</h1>
+    <small>Copyright &copy; Your Website</small>
+    <ul class="social-icons mb-md">
+        <li class="social-icons-fa-github"><a href="https://github.com" target="_blank" title="GitHub"><i class="fa fa-github"></i></a>
+        </li>
+        <li class="social-icons-instagram"><a href="www.linkedin.com" target="_blank" title="Linkedin"><i class="fa fa-instagram"></i></a>
+        </li>
+        <li class="social-icons-stack-overflow"><a href="http://stackoverflow.com" target="_blank" title="Linkedin"><i class="fa fa-stack-overflow"></i></a>
+        </li>
+    </ul>
+  </div>
+</footer>
