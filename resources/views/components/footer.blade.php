59b72dc9d8faeb82d4c89facecc92d10c464aa49
unaibarrasatxurdi <unaibarrasatxurdi@gmail.com>
2022-01-12 11:33:24 +0100

    FooterSigo

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,14 +1,28 @@
 <footer class="bg-dark text-light" id="footer">
-  <div class="container">
-    <h1>Zimmer</h1>
-    <small>Copyright &copy; Your Website</small>
-    <ul class="social-icons mb-md">
-        <li class="social-icons-fa-github"><a href="https://github.com/unaibarrasatxurdi/zimmerT4" target="_blank" title="GitHub"><i class="fa fa-github"></i></a>
-        </li>
-        <li class="social-icons-instagram"><a href="www.instagram.com" target="_blank" title="instagram"><i class="fa fa-instagram"></i></a>
-        </li>
-        <li class="social-icons-twitter"><a href="http://twitter.com" target="_blank" title="twitter"><i class="fa fa-twitter"></i></a>
-        </li>
-    </ul>
+  <div class="container d-flex justify-content-between flex-row" id="footerdivpadre">
+    <div>
+      <h1>Zimmer</h1>
+      <p>Zimmer zu vermieten</p>
+    </div>
+
+    <div class="d-flex align-items-center flex-column">
+      <p class="m-0" >Sare Sozialak</p>
+      <div class="pl-0">
+        <ul class="social-icons mb-md p-0">
+          <li class="social-icons-fa-github"><a href="https://github.com/unaibarrasatxurdi/zimmerT4" target="_blank" title="GitHub"><i class="fa fa-github"></i></a>
+          </li>
+          <li class="social-icons-instagram"><a href="http://instagram.com" target="_blank" title="instagram"><i class="fa fa-instagram"></i></a>
+          </li>
+          <li class="social-icons-twitter"><a href="http://twitter.com" target="_blank" title="twitter"><i class="fa fa-twitter"></i></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="d-flex align-self-center">
+      <small>Copyright &copy; Zimmer</small>
+    </div>
+
+
   </div>
 </footer>
