b58495e27b7a3ee9448c1079b9839fd18109c699
Mikel Martín <mikelmartin43@gmail.com>
2022-01-18 11:58:47 +0100

    footer icon margin

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -9,11 +9,11 @@
     <div class="d-flex align-self-center flex-column m-0">
       <div class="p-0">
         <ul class="social-icons mb-md p-0 m-0">
-          <li class="social-icons-fa-github"><a href="https://github.com/unaibarrasatxurdi/zimmerT4" target="_blank" title="GitHub"><i class="fa fa-github"></i></a>
+          <li class="social-icons-fa-github mx-2"><a href="https://github.com/unaibarrasatxurdi/zimmerT4" target="_blank" title="GitHub"><i class="fa fa-github"></i></a>
           </li>
-          <li class="social-icons-instagram"><a href="http://instagram.com" target="_blank" title="instagram"><i class="fa fa-instagram"></i></a>
+          <li class="social-icons-instagram mx-2"><a href="http://instagram.com" target="_blank" title="instagram"><i class="fa fa-instagram"></i></a>
           </li>
-          <li class="social-icons-twitter"><a href="http://twitter.com" target="_blank" title="twitter"><i class="fa fa-twitter"></i></a>
+          <li class="social-icons-twitter mx-2"><a href="http://twitter.com" target="_blank" title="twitter"><i class="fa fa-twitter"></i></a>
           </li>
         </ul>
       </div>
