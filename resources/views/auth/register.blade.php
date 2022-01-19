01f3468b38a4851586f601862c831a364b83816b
Mikel-Hormaza <73583125+Mikel-Hormaza@users.noreply.github.com>
2022-01-19 08:24:57 +0100

    Ocultar inicial del registro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,7 +10,7 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
-                        <input id="initials" type="text" name="initials">
+                        <input hidden id="initials" type="text" name="initials">
                         <div class="row mb-3">
                             <label for="name" class="col-md-4 col-form-label text-md-right">Izena</label>
 
