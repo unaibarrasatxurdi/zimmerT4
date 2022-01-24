0f8afc25cd7227b17c9530cf449713e716a8665f
lauraspe <73583118+lauraspe@users.noreply.github.com>
2022-01-24 10:52:44 +0100

    validacion
    modifico index.blade.php y app.blade.php
    hace las validaciones. Ahora estoy preparando el span de errores

--- resources/views/perfil/index.blade.php
+++ resources/views/perfil/index.blade.php
@@ -44,11 +44,14 @@
 
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button id="gordeBotoia" type="button" class="btn btn-primary">
                                     {{ __('Gorde') }}
                                 </button>
                             </div>
                         </div>
+                        <span>
+                            kaixo soy el span
+                        </span>
                     </form>
                 </div>
             </div>
@@ -57,10 +60,64 @@
 </div>
 @endif
 
+@section('scriptsErabiltzaile')
+    @parent
+ 
+    <script type="application/javascript">
+    class erabiltzailea {
+                constructor(p_inizialak, p_izena, p_email) {
+                    this._inizialak=p_inizialak;
+                    this._izena = p_izena;
+                    this._email=p_email;
+                }
+   
+                /* getterrak */
+                inizialak(){
+                    return this._inizialak;
+                }
+   
+                izena() {
+                    return this._izena;
+                }
+   
+                email(){
+                    return this._email
+                }
+   
+                /* inizialen luzeera gehienez 2 eta gutxienez 0 izatea eta letra larriz egotea konprobatu
+                baldintzak ez badira betetzen, return false */
+                balidatuInizialak(){
+                    let inizialak = this.inizialak();
+                    if((inizialak.length==2)||(inizialak.length==1)&&(inizialak === inizialak.toUpperCase())){
+                        return true;
+                    }else{
+                        return false;
+                    }
+                }
+   
+                /* izena soilik letrak izatea
+                baldintza ez bada betetzen, return false */
+                balidatuIzena(){
+                    let izena = this.izena();
+                    let patroia = /^[A-Za-z\s]+$/ /* \s-renik hutsuneak onartzen ditu */
+                    return patroia.test(izena);
+                }
+   
+                /* emaila emailaren formatua izatea. Bestela false */
+                balidatuEmaila(){
+                    let email = this.email();
+                    let patroia = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+                    return patroia.test(email);
+               
+                }
+            }
+ 
+    </script>
+@endsection
+
+
 <script type="application/javascript">
-
     $(document).ready(function() {
-
         $("#name").on("keyup", function() {
             let initials_list = $(this).val().split(" ");
             let initials = "";
@@ -71,41 +128,43 @@
                         break;
                 }
             }
+            $("#initials").val(initials);
         });
 
-/*         $("form#formulario").validate({
-            rules: {
-                initials : {
-                    required: true,
-                    minlength: 1,
-                    maxlenght: 2
-                },
-                name: {
-                    required: true,
-                },
-                email: {
-                    required: true,
-                    email: true
-                },
-            },
-            messages : {
-                initials: {
-                    required: "Errorea"
-                },
-                name: {
-                    required: "Mesedez, sartu izena"
-                },
-                email: {
-                    required: "Mesedez, sartu email-a",
-                    email: "Errorea email-aren formatuan"
-                }  
-            }
+        $("#gordeBotoia").on("click", function(){
+                let erabiltzaile=new erabiltzailea(
+                    $("#initials").val(),
+                    $("#name").val(),
+                    $("#email").val()
+                );
+                balidatuEtaBidaliFormularioa(erabiltzaile);
             });
-
-
-    }); */
     });
 
+        /* konprobatzaileak deitu. Konprobatzaileak true itzultzen badu, formularioa bidali*/
+        function balidatuEtaBidaliFormularioa(erabiltzaile){
+            if(konprobatuDatuGuztiak(erabiltzaile)){
+                $("#formulario").submit();
+                return true;
+            }else{
+                return false;
+            }
+        }
+
+        /* hiru inputetan testua dagoela konprobatu
+        bat hutsik badago - return false*/            
+        function konprobatuDatuGuztiak(erabiltzaileDatuak){
+            if((erabiltzaileDatuak.inizialak().length==0)||(!erabiltzaileDatuak.balidatuInizialak())){ 
+                return false;
+            }
+            if((erabiltzaileDatuak.izena().length==0)||(!erabiltzaileDatuak.balidatuIzena())){
+                return false;
+            }
+            if((erabiltzaileDatuak.email().length==0)||(!erabiltzaileDatuak.balidatuEmaila())){
+                return false;
+            }
+            return true;
+        }
 </script>
 
 @endsection
