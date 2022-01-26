e5c4fb0a462a535d740c77e020dabdc0506c0849
Mikel Martín <mikelmartin43@gmail.com>
2022-01-26 10:14:49 +0100

    iconos footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -9,12 +9,12 @@
     <div class="d-flex align-self-center flex-column m-0">
       <div class="p-0">
         <ul class="social-icons mb-md p-0 m-0">
-          <li class="social-icons-fa-github mx-2"><a href="https://github.com/unaibarrasatxurdi/zimmerT4" target="_blank" title="GitHub"><i class="fa fa-github"></i></a>
-          </li>
-          <li class="social-icons-instagram mx-2"><a href="http://instagram.com" target="_blank" title="instagram"><i class="fa fa-instagram"></i></a>
-          </li>
-          <li class="social-icons-twitter mx-2"><a href="http://twitter.com" target="_blank" title="twitter"><i class="fa fa-twitter"></i></a>
-          </li>
+          <li class="social-icons-fa-github mx-2"><a href="https://github.com/unaibarrasatxurdi/zimmerT4" target="_blank" title="GitHub"><i class="fa fa-github"></i></a></li>
+          <li class="social-icons-instagram mx-2"><a href="http://instagram.com" target="_blank" title="instagram"><i class="fa fa-instagram"></i></a></li>
+          <li class="social-icons-twitter mx-2"><a href="http://twitter.com" target="_blank" title="twitter"><i class="fa fa-twitter"></i></a></li>
+          <li class="social-icons-twitter mx-2"><a href="http://youtube.com" target="_blank" title="twitter"><i class="fa fa-youtube"></i></a></li>
+          <li class="social-icons-twitter mx-2"><a href="http://linkedin.com" target="_blank" title="twitter"><i class="fa fa-linkedin"></i></a></li>
+          <li class="social-icons-twitter mx-2"><a href="http://facebook.com" target="_blank" title="twitter"><i class="fa fa-facebook"></i></a></li>
         </ul>
       </div>
     </div>
