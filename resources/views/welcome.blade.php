b10cb551616a296901b007134482d193516c8f52
Mikel Martín <mikelmartin43@gmail.com>
2022-01-28 10:07:25 +0100

    index

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,19 +16,19 @@
     </div>
     <div class="destinations container d-flex justify-content-around flex-wrap my-5">
         <div>
-            <span class="d-block fw-bold">Araba</span>
+            <span class="text-center mb-2 d-block fw-bold">Araba</span>
             <a href="/hoteles?probintzia=araba">
                 <img src="img/gasteiz.jpg" width="300px" class="shadow-sm">
             </a>
         </div>
         <div>
-            <span class="d-block fw-bold">Bizkaia</span>
+            <span class="text-center mb-2 d-block fw-bold">Bizkaia</span>
             <a href="/hoteles?probintzia=bizkaia">
                 <img src="img/bilbo.jpg" width="300px" class="shadow-sm">
             </a>
         </div>
         <div>
-            <span class="d-block fw-bold">Gipuzkoa</span>
+            <span class="text-center mb-2 d-block fw-bold">Gipuzkoa</span>
             <a href="/hoteles?probintzia=gipuzkoa">
                 <img src="img/donosti.jpg" width="300px" class="shadow-sm">
             </a>
@@ -41,25 +41,25 @@
 
     <div class="motak container d-flex justify-content-around flex-wrap my-5" id="motak">
         <div>
-            <span class="d-block fw-bold">Hotel</span>
+            <span class="text-center mb-2 d-block fw-bold">Hotel</span>
             <a href="/hoteles?mota=Hoteles">
                 <img src="img/hotel.jpg" width="300px" class="shadow-sm">
             </a>
         </div>
         <div>
-            <span class="d-block fw-bold">Apartamento</span>
+            <span class="text-center mb-2 d-block fw-bold">Apartamento</span>
             <a href="/hoteles?mota=Apartamentos">
                 <img src="img/APARTAMENTO.jpg" width="300px" class="shadow-sm">
             </a>
         </div>
         <div>
-            <span class="d-block fw-bold">Apartahotel</span>
+            <span class="text-center mb-2 d-block fw-bold">Apartahotel</span>
             <a href="/hoteles?mota=Hotel-Apartamento">
                 <img src="img/apartahotel.jpg" width="300px" class="shadow-sm">
             </a>
         </div>
         <div>
-            <span class="d-block fw-bold">Pension</span>
+            <span class="text-center mb-2 d-block fw-bold">Pension</span>
             <a href="/hoteles?mota=Pensiones">
                 <img src="img/pennsion.jpg" width="300px" class="shadow-sm">
             </a>
