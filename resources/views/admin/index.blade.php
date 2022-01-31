d3011e390794f44a73e1d94ef4eba3b663134ff8
Mikel Martín <mikelmartin43@gmail.com>
2022-01-31 15:47:30 +0100

    iconos pagina de usuarios

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,15 +4,15 @@
 
 <div class="container my-5">
 
-    <h3 class="mb-3">Erabiltzaile zerrenda</h3>
+    <h3 class="mb-4">Erabiltzaile zerrenda</h3>
 
     @foreach($usuarios as $user)
-        <div class="d-flex justify-content-between align-items-center border rounded shadow-sm px-3 py-2">
+        <div class="d-flex justify-content-between align-items-center border rounded shadow-sm px-3 py-2 mb-3">
             <div class="d-flex align-items-center gap-3">
                 @if($user->rol == 1)
-                    <i class="fa fa-user-plus fs-2"></i>
+                    <i class="fa fa-user-plus fs-2 text-muted"></i>
                 @elseif($user->rol == 0)
-                    <i class="fa fa-user fs-2"></i>
+                    <i class="fa fa-user fs-2 text-muted"></i>
                 @endif
                 <div>
                     <div class="d-flex gap-2">
